733819ad318dc074a3a12578e94856380ef6808f
Mardari Dorel <doru.mardari@trisoft.ro>
2016-06-23 12:37:08 +0300

    Initial commit.

---
+++ src/AppBundle/Listener/ThemeRequestListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Listener;
+
+use Sylius\Bundle\ThemeBundle\Context\SettableThemeContext;
+use Sylius\Bundle\ThemeBundle\Repository\ThemeRepositoryInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+final class ThemeRequestListener
+{
+    /**
+     * @var ThemeRepositoryInterface
+     */
+    private $themeRepository;
+
+    /**
+     * @var SettableThemeContext
+     */
+    private $themeContext;
+
+    /**
+     * @param ThemeRepositoryInterface $themeRepository
+     * @param SettableThemeContext $themeContext
+     */
+    public function __construct(ThemeRepositoryInterface $themeRepository, SettableThemeContext $themeContext)
+    {
+        $this->themeRepository = $themeRepository;
+        $this->themeContext = $themeContext;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            // don't do anything if it's not the master request
+            return;
+        }
+
+        $this->themeContext->setTheme(
+            $this->themeRepository->findByLogicalName('sylius/example-theme')
+        );
+    }
+}
